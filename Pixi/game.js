5fba62267dd0637d59081a9042b8a4cc43b2210e
KHIDE DESKTOP <noonoox@gmail.com>
2018-04-26 20:52:49 -0700

    PROTOTYPE PHASE - Added grid - Added helper functions (unfinished) - Tweaked WhiteBox.png to be 1x1 to allow easy scaling numbers for prototyping

--- Pixi/game.js
+++ Pixi/game.js
@@ -7,21 +7,29 @@ function Init(){
 	// TODO: Loading bar
 
 	// Import textures
-	PIXI.loader.add("whiteBox", "images/whiteBox.png")
+	PIXI.loader.add("whiteBox", "images/WhiteBox.png")
 			   .load(Init2);
 }
 
 function Init2(){
 	// Proto{
 
-	sprite = new PIXI.Sprite(PIXI.loader.resources["whiteBox"].texture);
-	app.stage.addChild(sprite);
+	//sprite = GetSprite("whiteBox", 100, 100, .5, .5);
 	
-	sprite.anchor.set(.5, .5);
+
+	unit = app.renderer.width / 10;
+
+	for(let x = 0; x < 10; x++){
+		for(let y = 0; y < 10; y++){
+			GetSprite("whiteBox", x * unit, y * unit, 0, 0, unit, unit, (x + y) % 2 == 0 ? 0xEEEEEE : 0xEAEAEA);
+		}
+	}
+	
+	/*sprite.anchor.set(.5, .5);
 	sprite.x = 200;
 	sprite.y = 200;
 	sprite.vx = 0;
-	sprite.vy = 0;
+	sprite.vy = 0;*/
 
 	//	Proto}
 
@@ -29,7 +37,24 @@ function Init2(){
 }
 
 function Update(delta){ // Note: Runs/up to 60fps. Any real-world values should be multiplied by delta to scale properly w/ low FPS
-	sprite.x += sprite.vx * delta;
-	sprite.y += sprite.vy * delta;
 	mousePos = app.renderer.plugins.interaction.mouse.global;
+}
+
+function GetSprite(name, posX = 0, posY = 0, anchorX = 0, anchorY = 0, scaleX = 1, scaleY = 1, tint = 0xFFFFFF){
+	let sprite = new PIXI.Sprite(PIXI.loader.resources[name].texture);
+	sprite.position.set(posX, posY);
+	sprite.anchor.set(anchorX, anchorY);
+	sprite.scale.set(scaleX, scaleY);
+	sprite.tint = tint;
+	app.stage.addChild(sprite);
+	
+	return sprite;
+}
+
+function GetObj(){
+
+}
+
+function SetPos(){
+
 }
