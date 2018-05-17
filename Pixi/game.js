253bbd2169b8aa505ee955f4059dc43312c61132
Khide <noonoox@gmail.com>
2018-05-17 15:19:18 -0700

    - Increased food move speed - Added more popups

--- Pixi/game.js
+++ Pixi/game.js
@@ -57,8 +57,8 @@ const relPos = {
 
 const waves = [
    // FORMAT: time:TIME TO START IN SECONDS, type:foodTypes.ANY, startRate:FRAMES BETWEEN SPAWNING A NEW FOOD, endRate:AMOUNT OF FOOD SPAWNED IS LERPED BETWEEN startRate AND THIS, length:HOW LONG FOOD WILL BE SPAWNED
-	[{time: 2, type: foodTypes.FRUIT, startRate: 2.1, endRate: 2.1, length: 20, from: 0}],
-	[{time: 2, type: foodTypes.FRUIT, startRate: 2, endRate: 1.6, length: 40, from: 0}]
+   [{time: 2, type: foodTypes.FRUIT, startRate: 2.1, endRate: 2.1, length: 20, from: 0}],
+   [{time: 2, type: foodTypes.FRUIT, startRate: 2, endRate: 1.6, length: 40, from: 0}]
 ];
 
 var inProgress = [];
@@ -115,7 +115,8 @@ function ClosePauseMenu(){
 }
 
 const startLives = 100;
-const startMoney = 250;
+const startMoney = 25000;
+const sellRate = .25;
 
 function StartGame(){
 	Destroy(mmLogo);
@@ -183,7 +184,8 @@ function StartGame(){
 function StartGame2(){
 	stageImg = GetObj(GetSprite("stage0", 0, 0, 1, 1));
 	stageImg.interactive = true;
-	stageImg.buttonMode = true;
+	stageImg.buttonMode = false;
+	stageImg.on('pointerdown', function(){Destroy(toPlaceIcon); toPlaceIcon = false; TogglePlacemat(false);});
 	BuildPlacemat();
 	//stageImg.on('touchstart', function(e){mousePos.x = e.pageX; mousePosY = e.pageY; console.log("touch! (" + mousePos.x + " " + mousePos.y + ")")}, true);
 
@@ -206,7 +208,7 @@ function StartGame2(){
 
 	// Assuming one level
 	track = [{x:9, y:4}, {x:8, y:4}, {x:7, y:4}, {x:6, y:4}, {x:5, y:4}, {x:5, y:5}, {x:4, y:5}, {x:3, y:5}, {x:3, y:6}, {x:3, y:7}, {x:4, y:7}, {x:10, y:4}, {x:11, y:4}, {x:12, y:4}, {x:13, y:4}, {x:14, y:4}, {x:14, y:3}, {x:14, y:2}, {x:15, y:2}, {x:16, y:2}, {x:16, y:8}, {x:15, y:8}, {x:14, y:8}, {x:13, y:8}, {x:13, y:9}, {x:5, y:7}, {x:6, y:7}, {x:7, y:7}, {x:8, y:6}, {x:8, y:7}, {x:9, y:6}, {x:10, y:6}, {x:10, y:7}, {x:10, y:8}, {x:10, y:9}, {x:10, y:10}, {x:11, y:10}, {x:12, y:10}, {x:13, y:10}, {x:13, y:11}, {x:13, y:12}, {x:13, y:13}, {x:12, y:13}, {x:11, y:13}, {x:10, y:13}, {x:9, y:13}, {x:8, y:13}, {x:8, y:12}, {x:7, y:12}, {x:6, y:12}, {x:6, y:11}, {x:6, y:10}, {x:6, y:9}, {x:5, y:9}, {x:4, y:9}, {x:4, y:10}, {x:4, y:11}, {x:4, y:12}, {x:3, y:13}, {x:2, y:13}, {x:4, y:13}, {x:4, y:14}, {x:4, y:15}, {x:4, y:16}, {x:5, y:16}, {x:6, y:16}, {x:6, y:15}, {x:7, y:15}, {x:8, y:15}, {x:9, y:15}, {x:10, y:15}, {x:10, y:16}, {x:10, y:17}, {x:10, y:18}, {x:9, y:18}, {x:8, y:18}, {x:7, y:18}, {x:6, y:18}, {x:5, y:18}, {x:4, y:18}, {x:3, y:18}, {x:12, y:14}, {x:12, y:15}, {x:12, y:16}, {x:213, y:16}, {x:14, y:16}, {x:15, y:16}, {x:15, y:15}, {x:15, y:14}, {x:15, y:13}, {x:616, y:13}, {x:17, y:13}, {x:17, y:8}, {x:17, y:2}, {x:2, y:2}, {x:3, y:2}, {x:4, y:2}, {x:5, y:2}, {x:6, y:2}, {x:7, y:2}, {x:8, y:2}, {x:8, y:3}, {x:13, y:16}, {x:16, y:13}/*, {x:2, y:18}*/];
-	let trackV = [{x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:0, y:1}, {x:-1, y:0}, {x:-1, y:0}, {x:0, y:1}, {x:0, y:1}, {x:1, y:0}, {x:1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:0, y:1}, {x:0, y:1}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:0, y:1}, {x:0, y:1}, {x:1, y:0}, {x:1, y:0}, {x:1, y:0}, {x:1, y:0}, {x:0, y:-1}, {x:1, y:0}, {x:0, y:1}, {x:0, y:1}, {x:0, y:1}, {x:0, y:1}, {x:1, y:0}, {x:1, y:0}, {x:1, y:0}, {x:0, y:1}, {x:0, y:1}, {x:0, y:1}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:0, y:-1}, {x:-1, y:0}, {x:-1, y:0}, {x:0, y:-1}, {x:0, y:-1}, {x:0, y:-1}, {x:-1, y:0}, {x:-1, y:0}, {x:0, y:1}, {x:0, y:1}, {x:0, y:1}, {x:0, y:1}, {x:1, y:0}, {x:1, y:0}, {x:0, y:1}, {x:0, y:1}, {x:0, y:1}, {x:1, y:0}, {x:1, y:0}, {x:0, y:-1}, {x:1, y:0}, {x:1, y:0}, {x:1, y:0}, {x:1, y:0}, {x:0, y:1}, {x:0, y:1}, {x:0, y:1}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:0, y:-1}, {x:0, y:-1}, {x:0, y:-1}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:0, y:1}, {x:0, y:1}, {x:-1, y:0}, {x:0, y:1}, {x:-1, y:0}, {x:-1, y:0}, {x:-1, y:0}, {x:1, y:0}, {x:1, y:0}, {x:1, y:0}, {x:1, y:0}, {x:1, y:0}, {x:1, y:0}, {x:0, y:1}, {x:0, y:1}, {x:-1, y:0}, {x:-1, y:0}, /*{x:-1, y:0}*/];
+	let trackV = [{x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:0, y:1.5}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:0, y:1.5}, {x:0, y:1.5}, {x:1.5, y:0}, {x:1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:0, y:1.5}, {x:0, y:1.5}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:0, y:1.5}, {x:0, y:1.5}, {x:1.5, y:0}, {x:1.5, y:0}, {x:1.5, y:0}, {x:1.5, y:0}, {x:0, y:-1.5}, {x:1.5, y:0}, {x:0, y:1.5}, {x:0, y:1.5}, {x:0, y:1.5}, {x:0, y:1.5}, {x:1.5, y:0}, {x:1.5, y:0}, {x:1.5, y:0}, {x:0, y:1.5}, {x:0, y:1.5}, {x:0, y:1.5}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:0, y:-1.5}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:0, y:-1.5}, {x:0, y:-1.5}, {x:0, y:-1.5}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:0, y:1.5}, {x:0, y:1.5}, {x:0, y:1.5}, {x:0, y:1.5}, {x:1.5, y:0}, {x:1.5, y:0}, {x:0, y:1.5}, {x:0, y:1.5}, {x:0, y:1.5}, {x:1.5, y:0}, {x:1.5, y:0}, {x:0, y:-1.5}, {x:1.5, y:0}, {x:1.5, y:0}, {x:1.5, y:0}, {x:1.5, y:0}, {x:0, y:1.5}, {x:0, y:1.5}, {x:0, y:1.5}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:0, y:-1.5}, {x:0, y:-1.5}, {x:0, y:-1.5}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:0, y:1.5}, {x:0, y:1.5}, {x:-1.5, y:0}, {x:0, y:1.5}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:-1.5, y:0}, {x:1.5, y:0}, {x:1.5, y:0}, {x:1.5, y:0}, {x:1.5, y:0}, {x:1.5, y:0}, {x:1.5, y:0}, {x:0, y:1.5}, {x:0, y:1.5}, {x:-1.5, y:0}, {x:-1.5, y:0}, /*{x:-1.5, y:0}*/];
 	
 	let trackAnim = [];
 
@@ -288,7 +290,7 @@ function StartGame2(){
 			.on('pointerover', function(){animalsB.scale.x *= 1.2; animalsB.scale.y *= 1.2;})
 			.on('pointerout', function(){animalsB.scale.x /= 1.2; animalsB.scale.y /= 1.2;});
 	
-	animalsText = GetObj(new PIXI.Text("Animals: $770", hudStyle), animalsB.x, animalsB.y - unit, app.stage, relPos.IGNOREMARGIN);
+	animalsText = GetObj(new PIXI.Text("Feed: $770", hudStyle), animalsB.x, animalsB.y - unit, app.stage, relPos.IGNOREMARGIN);
 	animalsText.anchor.set(.5, .5);
 
 	purifierB = GetObj(GetSprite("purify", .5, .5, 1.25, 1.25), sidebarUnit * 5.77, uiMargin / 2, app.stage, relPos.SIDEBAR);
@@ -298,7 +300,7 @@ function StartGame2(){
 			 .on('pointerover', function(){purifierB.scale.x *= 1.2; purifierB.scale.y *= 1.2;})
 			 .on('pointerout', function(){purifierB.scale.x /= 1.2; purifierB.scale.y /= 1.2;});
 	
-	purifierText = GetObj(new PIXI.Text("Purifier: $900", hudStyle), purifierB.x, purifierB.y + unit, app.stage, relPos.IGNOREMARGIN);
+	purifierText = GetObj(new PIXI.Text("Purify: $900", hudStyle), purifierB.x, purifierB.y + unit, app.stage, relPos.IGNOREMARGIN);
 	purifierText.anchor.set(.5, .5);
 
 	factoryB = GetObj(GetSprite("factory", .5, .5, .9, .9), sidebarUnit * 6.95, uiMargin / 2, app.stage, relPos.SIDEBAR);
@@ -308,7 +310,7 @@ function StartGame2(){
 			.on('pointerover', function(){factoryB.scale.x *= 1.2; factoryB.scale.y *= 1.2;})
 			.on('pointerout', function(){factoryB.scale.x /= 1.2; factoryB.scale.y /= 1.2;});
 	
-	factoryText = GetObj(new PIXI.Text("Factory: $1200", hudStyle), factoryB.x, factoryB.y - unit, app.stage, relPos.IGNOREMARGIN);
+	factoryText = GetObj(new PIXI.Text("Process: $1200", hudStyle), factoryB.x, factoryB.y - unit, app.stage, relPos.IGNOREMARGIN);
 	factoryText.anchor.set(.5, .5);
 
 	wantToPlace = "";
@@ -339,7 +341,7 @@ function GetPlaceIcon(){
 	}
 
 	toPlaceIcon.alpha = .75;
-	TogglePlacemat();
+	TogglePlacemat(true);
 }
 
 const foodProcessDist = 4 * 4;
@@ -553,18 +555,20 @@ function PlaceTower(){
 				tower.atOnce = 5; // Amount of concurrent users
 				tower.processTime = 360; // Frames required to process one food item
 				tower.value = 80; // Amount of score and money gained when all maxes have been met
+				tower.cost = 250;
 			}
-		}else if(wantToPlace == towerTypes.ANIMALS){ // Ravenously devours meat
+		}else if(wantToPlace == towerTypes.ANIMALS){ // Consume meat & bread at a high rate
 			if(Buy(770)){
 				tower = GetObj(GetSprite("animals", .5, .5, 1.25, 1.25), x, y, app.stage, relPos.IGNOREMARGIN);
-				tower.allow = [foodTypes.MEAT];
+				tower.allow = [foodTypes.MEAT, foodTypes.BREAD];
 				tower.ignore = [];
 				tower.max = [4]; // If just one entry, then all entries in .allow will contribute towards the same max count, otherwise, individual maxes will be used
 				tower.finished = [0]; // MUST contain a 0 for every entry in .max[]
 				tower.curr = [[]]; // MUST contain an empty array for every entry in .max[]
 				tower.atOnce = 4; // Amount of concurrent users
-				tower.processTime = 20; // Frames required to process one food item
+				tower.processTime = 80; // Frames required to process one food item
 				tower.value = 100; // Amount of score and money gained when all maxes have been met
+				tower.cost = 770;
 			}
 		}else if(wantToPlace == towerTypes.FACTORY){ // A mass-processing machine that accepts anything but meat, only processing a few at a time. Isn't very profitable
 			if(Buy(1200)){
@@ -577,6 +581,7 @@ function PlaceTower(){
 				tower.atOnce = 2; // Amount of concurrent users
 				tower.processTime = 30; // Frames required to process one food item
 				tower.value = 250; // Amount of score and money gained when all maxes have been met
+				tower.cost = 1200;
 			}
 		}else if(wantToPlace == towerTypes.DONATION){ // More expensive & picky than composting, but is faster & worth more
 			if(Buy(400)){
@@ -589,6 +594,7 @@ function PlaceTower(){
 				tower.atOnce = 1; // Amount of concurrent users
 				tower.processTime = 45; // Frames required to process one food item
 				tower.value = 60; // Amount of score and money gained when all maxes have been met
+				tower.cost = 400;
 			}
 		}else if(wantToPlace == towerTypes.RECYCLE){
 			if(Buy(650)){
@@ -601,6 +607,7 @@ function PlaceTower(){
 				tower.atOnce = 1; // Amount of concurrent users
 				tower.processTime = 60; // Frames required to process one food item
 				tower.value = 175; // Amount of score and money gained when all maxes have been met
+				tower.cost = 650;
 			}
 		}else if(wantToPlace == towerTypes.PURIFIER){
 			if(Buy(900)){
@@ -613,10 +620,13 @@ function PlaceTower(){
 				tower.atOnce = 1; // Amount of concurrent users
 				tower.processTime = 20; // Frames required to process one food item
 				tower.value = 225; // Amount of score and money gained when all maxes have been met
+				tower.cost = 900;
 			}
 		}
 
 		if(tower != false){
+			GetPopup("-" + tower.cost, tower.x, tower.y - unit * 3 / 4, 1, 1, 0xFF0000);
+
 			tower.type = wantToPlace;
 			tower.currCount = 0;
 			console.log("Tower placed at " + tower.x + " " + tower.y);
@@ -628,8 +638,8 @@ function PlaceTower(){
 			tower.buttonMode = true;
 			tower.on('pointerdown', function(){
 						towers.splice(towers.indexOf(this), 1);
-						AdjustMoney(this.value);
-						GetPopup("+" + this.value, this.x, this.y - unit * 3 / 4, 1, 1, 0x00FF00);
+						AdjustMoney(Math.ceil(this.cost * sellRate));
+						GetPopup("+" + Math.ceil(this.cost * sellRate), this.x, this.y - unit * 3 / 4, 1, 1, 0x00FF00);
 
 						Destroy(this.finish);
 						Destroy(this.progress);
@@ -641,8 +651,6 @@ function PlaceTower(){
 			towers.push(tower);
 		}
 	}
-
-
 }
 
 function Buy(cost){
@@ -676,7 +684,23 @@ function BuildPlacemat(){ // Helps laying spaces
 }
 
 function TogglePlacemat(){
-	if(placemat[0].alpha < .2){
+	if(placemat[0].alpha < .2 || forceOn){
+		for(let i = 0; i < placemat.length; i++){
+			placemat[i].alpha = .666 / 2;
+			placemat[i].interactive = true;
+			placemat[i].buttonMode = true;
+		}
+	}else{
+		for(let i = 0; i < placemat.length; i++){
+			placemat[i].alpha = 0;
+			placemat[i].interactive = false;
+			placemat[i].buttonMode = false;
+		}
+	}
+}
+
+function TogglePlacemat(forceTo){
+	if(forceTo){
 		for(let i = 0; i < placemat.length; i++){
 			placemat[i].alpha = .666 / 2;
 			placemat[i].interactive = true;
